0642b05b4bfb1f6ab9f791cfc8845a6aa199f17a
cheesekaek <karthika.anusurek@gmail.com>
2026-05-16 10:50:10 -0400

    build: spec class for READ in service layer

---
+++ src/main/java/com/example/libraryapi/specification/BookSpecification.java
@@ -0,0 +1,28 @@
+package com.example.libraryapi.specification;
+
+import com.example.libraryapi.entity.Book;
+import org.springframework.data.jpa.domain.Specification;
+
+public class BookSpecification {
+    public static Specification<Book> likeTitle(String title) {
+        return (root, query, criteriaBuilder) ->
+                criteriaBuilder.like(root.get("title"), "%" + title + "%");
+        // within col named 'title', look for patter with given string title
+    }
+
+    public static Specification<Book> likeAuthor(String author) {
+        return (root, query, criteriaBuilder) ->
+                criteriaBuilder.like(root.get("author"), "%" + author + "%");
+    }
+
+    public static Specification<Book> likeCategory(String category) {
+        return (root, query, criteriaBuilder) ->
+                criteriaBuilder.like(root.get("category"), "%" + category + "%");
+    }
+
+    public static Specification<Book> likeYear(int year) {
+        return (root, query, criteriaBuilder) ->
+                criteriaBuilder.equal(root.get("year"), year);
+        // specific year
+    }
+}
