28d45e26af9b39416f0030d79687734fd360af8a
cheesekaek <karthika.anusurek@gmail.com>
2026-05-18 08:20:22 -0400

    fix: getters setters add to book create response dto

--- src/main/java/com/example/libraryapi/dto/book/BookCreateResponse.java
+++ src/main/java/com/example/libraryapi/dto/book/BookCreateResponse.java
@@ -5,8 +5,10 @@ import jakarta.validation.constraints.Positive;
 import jakarta.validation.constraints.PositiveOrZero;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
+import lombok.Data;
 import lombok.NoArgsConstructor;
 
+@Data
 @AllArgsConstructor
 @NoArgsConstructor
 @Builder
