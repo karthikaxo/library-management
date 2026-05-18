6e890c07592a01fd9de9244b6edc553c11960470
cheesekaek <karthika.anusurek@gmail.com>
2026-05-18 08:20:57 -0400

    build: add book repository method for finding ISBN

--- src/main/java/com/example/libraryapi/repository/BookRepository.java
+++ src/main/java/com/example/libraryapi/repository/BookRepository.java
@@ -8,5 +8,8 @@ import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 public interface BookRepository extends JpaRepository<Book, Long>,
         JpaSpecificationExecutor<Book> {
     boolean existsByIsbn(String isbn);
+
+    Book findByIsbn(String isbn);
+
     // specifications for dynamic queries
 }
