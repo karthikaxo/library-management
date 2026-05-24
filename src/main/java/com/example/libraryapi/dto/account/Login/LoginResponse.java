9d004be3a448abdea417f126fdbbdd26d0de9d31
cheesekaek <karthika.anusurek@gmail.com>
2026-05-24 11:02:14 -0400

    build: login dtos

---
+++ src/main/java/com/example/libraryapi/dto/account/Login/LoginResponse.java
@@ -0,0 +1,14 @@
+package com.example.libraryapi.dto.account.Login;
+
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class LoginResponse {
+    @NotNull
+    private String token;
+}
