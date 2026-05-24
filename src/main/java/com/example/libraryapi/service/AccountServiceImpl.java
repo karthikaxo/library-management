053c3426b9a6dfc887f871d302be15f5d7ef67b4
cheesekaek <karthika.anusurek@gmail.com>
2026-05-24 11:00:09 -0400

    build: update account service layer with login verif

--- src/main/java/com/example/libraryapi/service/AccountServiceImpl.java
+++ src/main/java/com/example/libraryapi/service/AccountServiceImpl.java
@@ -3,6 +3,8 @@ package com.example.libraryapi.service;
 
 import com.example.libraryapi.dto.account.LibrarianCreate.LibrarianCreateRequest;
 import com.example.libraryapi.dto.account.LibrarianCreate.LibrarianCreateResponse;
+import com.example.libraryapi.dto.account.Login.LoginRequest;
+import com.example.libraryapi.dto.account.Login.LoginResponse;
 import com.example.libraryapi.dto.account.MemberCreate.MemberCreateRequest;
 import com.example.libraryapi.dto.account.MemberCreate.MemberCreateResponse;
 import com.example.libraryapi.entity.Account;
@@ -11,6 +13,10 @@ import com.example.libraryapi.entity.Role;
 import com.example.libraryapi.mapper.AccountMapper;
 import com.example.libraryapi.repository.AccountRepository;
 import com.example.libraryapi.repository.LibraryRepository;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
@@ -24,11 +30,17 @@ public class AccountServiceImpl {
 
     private final LibraryRepository libraryRepository;
 
+    private final JWTService jwtService;
+
     private final BCryptPasswordEncoder encoder = new BCryptPasswordEncoder(7); // strength = 5
 
-    public AccountServiceImpl(AccountRepository accountRepository, LibraryRepository libraryRepository) {
+    private final AuthenticationManager authManager;
+
+    public AccountServiceImpl(AccountRepository accountRepository, LibraryRepository libraryRepository, JWTService jwtService, AuthenticationManager authManager) {
         this.accountRepository = accountRepository;
         this.libraryRepository = libraryRepository;
+        this.jwtService = jwtService;
+        this.authManager = authManager;
     }
 
     // ============================== CREATE ==============================
@@ -51,7 +63,7 @@ public class AccountServiceImpl {
 
         account.setUsername(createRequest.getUsername());
         account.setEmail(createRequest.getEmail());
-        account.setPassword(encoder.encode(account.getPassword())); // store hashed password
+        account.setPassword(encoder.encode(createRequest.getPassword())); // store hashed password
         account.setRole(Role.LIBRARIAN); // default
         library.addAccount(account);
         account.setCreateDate(LocalDate.now()); // default
@@ -76,7 +88,7 @@ public class AccountServiceImpl {
 
         account.setUsername(createRequest.getUsername());
         account.setEmail(createRequest.getEmail());
-        account.setPassword(encoder.encode(account.getPassword()));
+        account.setPassword(encoder.encode(createRequest.getPassword()));
         account.setRole(Role.MEMBER);
         account.setCreateDate(LocalDate.now());
         account.setLendings(new ArrayList<>()); // default
@@ -87,4 +99,28 @@ public class AccountServiceImpl {
     }
 
 
+    public LoginResponse login(LoginRequest loginRequest) {
+
+        // authenticate username/password
+        Authentication authentication =
+                authManager.authenticate(new UsernamePasswordAuthenticationToken(
+                        loginRequest.getUsername(),
+                        loginRequest.getPassword()
+                ));
+
+        if (authentication.isAuthenticated()) {
+            Account account = accountRepository.findByUsername(loginRequest.getUsername());
+            if (account == null) throw new UsernameNotFoundException("User not found.");
+
+            account.setLastLogin(LocalDate.now());
+            accountRepository.save(account);
+
+            String token = jwtService.generateToken(account);
+
+            return new LoginResponse(token);
+        };
+
+        // login failed
+        return new LoginResponse("");
+    }
 }
