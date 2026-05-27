a7b27b82cd965a506c95f23046f673bf925ef30f
cheesekaek <karthika.anusurek@gmail.com>
2026-05-27 13:16:09 -0400

    fix: add constructors for Fine

--- src/main/java/com/example/libraryapi/entity/Fine.java
+++ src/main/java/com/example/libraryapi/entity/Fine.java
@@ -1,9 +1,15 @@
 package com.example.libraryapi.entity;
 
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.time.LocalDate;
 
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 @Entity
 @Table
 public class Fine {
